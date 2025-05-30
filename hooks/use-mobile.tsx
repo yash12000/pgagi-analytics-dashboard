c01cbdba23095452670e72941798606564d00163
Yash Janbandhu <yashjanbandhu12@gmail.com>
2025-05-30 12:56:56 +0530

    feat: implement useMobile hook for responsive design

---
+++ hooks/use-mobile.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
+export function useMobile() {
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    const checkIfMobile = () => {
+      setIsMobile(window.innerWidth < 768)
+    }
+
+    checkIfMobile()
+
+    window.addEventListener("resize", checkIfMobile)
+
+    return () => {
+      window.removeEventListener("resize", checkIfMobile)
+    }
+  }, [])
+
+  return isMobile
+}
